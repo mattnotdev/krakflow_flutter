8087ade054dccf15ae93d8d5e9ab30ba7c5badc1
matt <mattnotdev@gmail.com>
2026-03-24 18:14:07 +0100

    simplified the app for learning purposes

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'A to-do app made for university students.',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'KrakFlow'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Text("KrakFlow"),
-            Text("Organizacja studiów"),
-            Text("Dzisiejsze zadania"),
-          ]
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text("KrakFlow"),
         ),
-      ),
+        body: Center(
+          child: Column(
+            children: [
+              Text("KrakFlow"),
+              Text("Organizacja Studiów"),
+              Text("Dzisiejsze zadania"),
+            ],
+          )
+        ),
+      )
     );
   }
+}
+
+class Task {
+  final String title;
+  final String deadline;
+
+  Task({required this.title, required this.deadline});
 }
